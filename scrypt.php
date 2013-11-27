39a2bda5cd3b191788b6eced9637589444bf8bd9
Mark Garrett <mgarrett@moderndeveloperllc.com>
2013-11-27 13:05:04 -0600

    Utilize @ircmaxell function from password_compat to generate good salt. PSR-2 formatting

--- scrypt.php
+++ scrypt.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file contains an example helper classes for the php-scrypt extension.
  *
@@ -22,17 +23,19 @@
  * You can check a password by calling Password::check($password, $hash)
  *
  * @category Security
- * @package  Scrypt
- * @author   Dominic Black <thephenix@gmail.com>
- * @license  http://www.opensource.org/licenses/BSD-2-Clause BSD 2-Clause License
- * @link     http://github.com/DomBlack/php-scrypt
+ * @package Scrypt
+ * @author Dominic Black <thephenix@gmail.com>
+ * @license http://www.opensource.org/licenses/BSD-2-Clause BSD 2-Clause License
+ * @link http://github.com/DomBlack/php-scrypt
  */
 class Password
 {
+
     /**
+     *
      * @var int The key length
      */
-    private static $_keyLength = 32;
+    private static $keyLength = 32;
 
     /**
      * Generates a random salt
@@ -43,13 +46,44 @@ class Password
      */
     public static function generateSalt($length = 8)
     {
-        $salt = '';
-        $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#%&*?';
-        $num = strlen($chars) - 1;
-
-        for ($i = 0; $i < $length; $i++) {
-            $salt .= $chars[mt_rand(0, $num)];
+        $buffer = '';
+        $buffer_valid = false;
+        if (function_exists('mcrypt_create_iv') && ! defined('PHALANGER')) {
+            $buffer = mcrypt_create_iv($length, MCRYPT_DEV_URANDOM);
+            if ($buffer) {
+                $buffer_valid = true;
+            }
         }
+        if (! $buffer_valid && function_exists('openssl_random_pseudo_bytes')) {
+            $cryptoStrong = false;
+            $buffer = openssl_random_pseudo_bytes($length, $cryptoStrong);
+            if ($buffer && $cryptoStrong) {
+                $buffer_valid = true;
+            }
+        }
+        if (! $buffer_valid && is_readable('/dev/urandom')) {
+            $f = fopen('/dev/urandom', 'r');
+            $read = strlen($buffer);
+            while ($read < $length) {
+                $buffer .= fread($f, $length - $read);
+                $read = strlen($buffer);
+            }
+            fclose($f);
+            if ($read >= $length) {
+                $buffer_valid = true;
+            }
+        }
+        if (! $buffer_valid || strlen($buffer) < $length) {
+            $bl = strlen($buffer);
+            for ($i = 0; $i < $length; $i ++) {
+                if ($i < $bl) {
+                    $buffer[$i] = $buffer[$i] ^ chr(mt_rand(0, 255));
+                } else {
+                    $buffer .= chr(mt_rand(0, 255));
+                }
+            }
+        }
+        $salt = str_replace('+', '.', base64_encode($buffer));
 
         return $salt;
     }
@@ -58,10 +92,10 @@ class Password
      * Create a password hash
      *
      * @param string $password The clear text password
-     * @param string $salt     The salt to use, or null to generate a random one
-     * @param int    $N        The CPU difficultly (must be a power of 2,  > 1)
-     * @param int    $r        The memory difficultly
-     * @param int    $p        The parallel difficultly
+     * @param string $salt The salt to use, or null to generate a random one
+     * @param int $N The CPU difficultly (must be a power of 2, > 1)
+     * @param int $r The memory difficultly
+     * @param int $p The parallel difficultly
      *
      * @return string The hashed password
      */
@@ -70,31 +104,33 @@ class Password
         if ($salt === false) {
             $salt = self::generateSalt();
         } else {
-            //Remove dollar signs from the salt, as we use that as a separator.
+            // Remove dollar signs from the salt, as we use that as a separator.
             $salt = str_replace('$', '', $salt);
         }
 
-        $hash = scrypt($password, $salt, $N, $r, $p, self::$_keyLength);
+        $hash = scrypt($password, $salt, $N, $r, $p, self::$keyLength);
 
-        return $N.'$'.$r.'$'.$p.'$'.$salt.'$'.$hash;
+        return $N . '$' . $r . '$' . $p . '$' . $salt . '$' . $hash;
     }
 
     /**
      * Check a clear text password against a hash
      *
-     * @param string $password The clear text password
-     * @param string $hash     The hashed password
+     * @param string $password
+     *            The clear text password
+     * @param string $hash
+     *            The hashed password
      *
      * @return boolean If the clear text matches
      */
     public static function check($password, $hash)
     {
         // Is there actually a hash?
-        if (!strlen($hash)) {
+        if (! strlen($hash)) {
             return false;
         }
 
-        list($N, $r, $p, $salt, $hash) = explode('$', $hash);
+        list ($N, $r, $p, $salt, $hash) = explode('$', $hash);
 
         // No empty fields?
         if (empty($N) or empty($r) or empty($p) or empty($salt) or empty($hash)) {
@@ -102,11 +138,11 @@ class Password
         }
 
         // Are numeric values numeric?
-        if (!is_numeric($N) or !is_numeric($r) or !is_numeric($p)) {
+        if (! is_numeric($N) or ! is_numeric($r) or ! is_numeric($p)) {
             return false;
         }
 
-        $calculated = scrypt($password, $salt, $N, $r, $p, self::$_keyLength);
+        $calculated = scrypt($password, $salt, $N, $r, $p, self::$keyLength);
 
         // Use compareStrings to avoid timeing attacks
         return self::compareStrings($hash, $calculated);
@@ -115,33 +151,32 @@ class Password
     /**
      * Zend Framework (http://framework.zend.com/)
      *
-     * @link      http://github.com/zendframework/zf2 for the canonical source repository
+     * @link http://github.com/zendframework/zf2 for the canonical source repository
      * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
-     * @license   http://framework.zend.com/license/new-bsd New BSD License
+     * @license http://framework.zend.com/license/new-bsd New BSD License
      *
+     *          Compare two strings to avoid timing attacks
      *
-     * Compare two strings to avoid timing attacks
+     *          C function memcmp() internally used by PHP, exits as soon as a difference
+     *          is found in the two buffers. That makes possible of leaking
+     *          timing information useful to an attacker attempting to iteratively guess
+     *          the unknown string (e.g. password).
      *
-     * C function memcmp() internally used by PHP, exits as soon as a difference
-     * is found in the two buffers. That makes possible of leaking
-     * timing information useful to an attacker attempting to iteratively guess
-     * the unknown string (e.g. password).
-     *
-     * @param  string $expected
-     * @param  string $actual
+     * @param string $expected
+     * @param string $actual
      *
      * @return boolean If the two strings match.
      */
     public static function compareStrings($expected, $actual)
     {
-        $expected     = (string) $expected;
-        $actual       = (string) $actual;
-        $lenExpected  = strlen($expected);
-        $lenActual    = strlen($actual);
-        $len          = min($lenExpected, $lenActual);
+        $expected = (string) $expected;
+        $actual = (string) $actual;
+        $lenExpected = strlen($expected);
+        $lenActual = strlen($actual);
+        $len = min($lenExpected, $lenActual);
 
         $result = 0;
-        for ($i = 0; $i < $len; $i++) {
+        for ($i = 0; $i < $len; $i ++) {
             $result |= ord($expected[$i]) ^ ord($actual[$i]);
         }
         $result |= $lenExpected ^ $lenActual;
