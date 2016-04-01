284a404876df33bd0703c47305d81811feb1a99b
Michael Armbruster <armbrust@fim.uni-passau.de>
2016-04-01 21:40:53 +0200

    Changed RETURN_STRINGL parameters for PHP7 support according to examples from https://wiki.php.net/phpng-upgrading

--- php_scrypt.c
+++ php_scrypt.c
@@ -199,10 +199,10 @@ PHP_FUNCTION(scrypt)
         php_hash_bin2hex(hex, buf, keyLength);
         efree(buf);
         hex[keyLength*2] = '\0';
-        RETURN_STRINGL(hex, keyLength * 2, 0);
+        RETURN_STRINGL(hex, keyLength * 2);
     } else {
         buf[keyLength] = '\0';
-        RETURN_STRINGL((char *)buf, keyLength, 0);
+        RETURN_STRINGL((char *)buf, keyLength);
     }
 }
 /* }}} */
